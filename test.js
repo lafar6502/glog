67a5f72a40a446197fc927abb6982c49f68e2b76
lafar6502 <rafal.gwizdala@gmail.com>
2015-04-06 11:12:20 +0200

    some experiments with inserts - very slow!1!!!!!

--- test.js
+++ test.js
@@ -4,5 +4,7 @@ var lr = require('./lib/logrepo');
 
 var r = lr.openLogRepository({fileName: 'test.logz', readOnly: false, logLevel: 'DEBUG'});
 
-r.addLog('localhost', 'test.js', 'INFO', 'this is test message');
+for (var i=0; i<30000; i++) {
+    r.addLog('localhost', 'test.js', 'INFO', 'this is test message #' + i);
+};
 r.close();
