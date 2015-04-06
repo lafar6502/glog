4f0be67bc54709222450ae7b073779c845d2c3e1
lafar6502 <rafal.gwizdala@gmail.com>
2015-04-06 11:58:45 +0200

    log rotation

--- test.js
+++ test.js
@@ -2,9 +2,17 @@ var sqli = require('sqlite3').verbose();
 
 var lr = require('./lib/logrepo');
 
-var r = lr.openLogRepository({fileName: 'test.logz', readOnly: false, logLevel: 'DEBUG'});
+var r = lr.openLogRepository({fileName: 'test_', fileDateFormat: 'YYYY-MM-DD_HHmmss_', readOnly: false, logLevel: 'DEBUG'});
 
-for (var i=0; i<30000; i++) {
-    r.addLog('localhost', 'test.js', 'INFO', 'this is test message #' + i);
-};
-r.close();
+var cnt = 0;
+var itv = setInterval(function() {
+    for (var i=0; i<1000; i++) {
+        r.addLog('localhost', 'test.js', 'INFO', 'this is test message #' + i);
+    };
+    cnt++;
+    if (cnt >= 10) {
+        clearInterval(itv);
+        r.close();
+        r = null;
+    };
+}, 2000);
