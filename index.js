65a91947a7a0f55b55459c8d711f75cffdac07c5
lafar6502 <rafal.gwizdala@gmail.com>
2015-04-07 22:05:55 +0200

    sv

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var lrepo = require('./lib/logrepo');
 
 module.exports = {
     openLogRepository: lrepo.openLogRepository,
+    openLogSearcher: lrepo.openLogSearcher,
     createLogCollector: lr.createLogCollector
 };
